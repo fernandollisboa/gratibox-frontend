df3ffd8b63ee9876ab420a7718678a456055eb2e
fernandollisboa <fernando.costa@ccc.ufcg.edu.br>
2021-11-23 00:09:38 -0300

    add: datas agora sim funcionando

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import { Outlet, Navigate } from "react-router-dom";
-const ProtectedRoute = () => {
+function ProtectedRoute({ children }) {
 	const token = localStorage.getItem("token");
 	return token ? <Outlet /> : <Navigate to="/" />;
-};
+}
 
 export default ProtectedRoute;
